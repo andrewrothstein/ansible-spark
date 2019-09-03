8eae8c8a4fba41de91429a28616c28472b78811f
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-09-03 09:42:45 -0500

    up to v2.4.4

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER=2.4.3
+VER=${1:-2.4.4}
 DIR=~/Downloads
 MIRROR=https://archive.apache.org/dist/spark/spark-${VER}
 
@@ -7,9 +7,11 @@ dl()
 {
     HADOOP=$1
     URL=${MIRROR}/spark-${VER}-bin-${HADOOP}.tgz.sha512
-    printf "# %s\n%s: sha512:%s\n" $URL $HADOOP `curl -sSL $URL | tr -d '\n' | tr -d ' ' | awk -F: '{print $2}'`
+    printf "    # %s\n" $URL
+    printf "    %s: sha512:%s\n" $HADOOP `curl -sSL $URL | tr -d '\n' | tr -d ' ' | awk -F: '{print $2}'`
 }
 
+printf "  '%s':\n" $VER
 dl hadoop2.6
 dl hadoop2.7
 
