762faa320c6271cf8f09ff9423343f84a3143f9a
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-06-27 15:40:54 -0400

    spark:3.3.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -9,15 +9,16 @@ dl()
     local hadoop_ver=$2
     local url=${MIRROR}/spark-${ver}/spark-${ver}-bin-${hadoop_ver}.tgz.sha512
     printf "    # %s\n" $url
-    printf "    %s: sha512:%s\n" $hadoop_ver $(curl -sSLf $url | tr -d '\n' | tr -d ' ' | awk -F: '{print $2}')
+    printf "    %s: sha512:%s\n" $hadoop_ver $(curl -sSLf $url | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver hadoop2.7
-    dl $ver hadoop3.2
+    dl $ver hadoop2
+    dl $ver hadoop3-scala2.13
+    dl $ver hadoop3
     dl $ver without-hadoop
 }
 
-dl_ver ${1:-3.2.1}
+dl_ver ${1:-3.3.0}
